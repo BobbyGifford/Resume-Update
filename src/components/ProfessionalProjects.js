48b8f1374e8423a221f679389157b99c5ec7c9d9
juice <Bobby>
2018-05-09 17:46:49 -0700

    Sizing on logos fixed.

--- src/components/ProfessionalProjects.js
+++ src/components/ProfessionalProjects.js
@@ -22,18 +22,20 @@ const ProfessionalProjects = () => {
                         <div className="card-text">
                             Some quick example text to build on the card title
                             and make up the bulk of the card's content.
-                            <br/>
+                            <br />
+                            <br />
+
+                            <div className="card-header">
+                                Notable Tech Used
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">React</li>
+                                <li className="list-group-item">Node</li>
+                                <li className="list-group-item">Bootstrap</li>
+                            </ul>
 
                         </div>
                     </div>
-                    <div className="card-header">
-                        Notable Tech Used
-                    </div>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">React</li>
-                        <li className="list-group-item">Node</li>
-                        <li className="list-group-item">Bootstrap</li>
-                    </ul>
                 </div>
 
                 {/*Card 2 begins*/}
@@ -42,17 +44,22 @@ const ProfessionalProjects = () => {
                     <img className="card-img-top" src={featureImage} alt="Card image cap"/>
                     <div className="card-body">
                         <h5 className="card-title">Kuku's Corner</h5>
-                        <div className="card-text">Some quick example text to build on
+                        <div className="card-text">
+                            Some quick example text to build on
                             the card title and make up the bulk of the card's content.
+                            <br />
+                            <br />
+
+                            <div className="card-header">
+                                Notable Tech Used
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">React</li>
+                                <li className="list-group-item">Bootstrap</li>
+                            </ul>
+
                         </div>
                     </div>
-                    <div className="card-header">
-                        Notable Tech Used
-                    </div>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">React</li>
-                        <li className="list-group-item">Bootstrap</li>
-                    </ul>
 
                 </div>
             </div>
