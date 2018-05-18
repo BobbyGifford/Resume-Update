9a983803c435d535beb9564f53e7395f7bf60d4d
juice <Bobby>
2018-05-18 12:10:51 -0700

    Added Marvel project.

---
+++ src/components/PersonalProjects/Marvel-Heroes.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import featureImage from '../../images/marvelapi.png'
+
+const MarvelHeroes = () => {
+    return (
+        <div className="card col-md-5 mb-3 mx-auto" style={{width: "18rem"}}>
+            <a href="http://heroes-app.s3-website-us-west-1.amazonaws.com/">
+            <img className="card-img-top" src={featureImage} alt="MarvelAPI"/>
+            </a>
+
+            <div className="card-body">
+                <h4 className="card-title">Marvel Api
+                </h4>
+
+
+                <div className="card-text">
+                    A site that takes advantage of the official Marvel API
+                    <br/>
+                    <br/>
+
+                    <div className="card-header">
+                        <h6>Notable Tech Used:
+                            <span className="badge badge-info ml-2">React</span>
+                            <span className="badge badge-warning ml-2">Marvel API</span>
+
+                            <span className="badge ml-2"
+                                  style={{backgroundColor: "purple", color: 'white'}}>Materialize CSS</span>
+                            <span className="badge ml-2"
+                                  style={{backgroundColor: "#4968AF", color: 'white'}}>AWS</span>
+                        </h6>
+
+                    </div>
+                    <div className={"text-center mt-2"}>
+                        <a href={"http://heroes-app.s3-website-us-west-1.amazonaws.com/"} className={"btn btn-info"}
+                           style={{color: "white"}}>View</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default MarvelHeroes;
--- src/components/PersonalProjects/PersonalProjects.js
+++ src/components/PersonalProjects/PersonalProjects.js
@@ -3,6 +3,7 @@ import GuitarInfo from "./GuitarInfo"
 import ClarifaiApi from "./ClarifaiApi"
 import OAuthApp from "./OAuthApp"
 import SportsPredictions from "./SportsPredictions"
+import MarvelHeroes from './Marvel-Heroes'
 
 const PersonalProjects = () => {
     return (
@@ -10,6 +11,7 @@ const PersonalProjects = () => {
             <h3 className={"text-center my-4"}>Personal Projects</h3>
             <div className="row" style={{color: "black"}}>
 
+                <MarvelHeroes/>
                 <GuitarInfo/>
                 <ClarifaiApi/>
                 <OAuthApp/>
