39752b99b4f17e5e51133abc589a129c94c6bf80
Bobby <bobby077joe@gmail.com>
2018-07-26 16:16:22 -0700

    Removed buttons. All projects show all the time now.

--- src/components/PersonalProjects/AncientArchitecture.js
+++ src/components/PersonalProjects/AncientArchitecture.js
@@ -13,9 +13,12 @@ const GuitarInfo = () => {
 
 
                 <div className="card-text">
+                <p className="text-danger">This app is hosted on Heroku and may take a few seconds to load. Please be patient.</p>
                     I made both the backend and frontend of this site from scratch. It is one fullstack application that I have been 
-                    (and currently am) still working on. I used React, Redux, and Bootstrap for the frontend. The backend uses Node, Express, and Mongo
-                    A site inspired by ancient structures of unknown construction and origin. I am a 
+                    (and currently am) still working on. I used React, Redux, and Bootstrap for the frontend. The backend uses Node, Express, and Mongo.
+                    <br />
+                    <br />
+                    This site is inspired by ancient structures of unknown construction and origin. I am a 
                     huge fan of Graham Hancock and I wanted to make a site for people who may be fans of him. The purpose 
                     this site is for people to share mysterious locations as well as their personal opinions on them. 
                     <br/>
